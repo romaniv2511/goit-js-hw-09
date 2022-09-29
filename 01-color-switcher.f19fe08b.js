55ddf5c12adb4a4cdab56c95a27ac7c6dfd314af
romaniv2511 <104019655+romaniv2511@users.noreply.github.com>
2022-09-29 17:08:43 +0000

    Deploying to gh-pages from @ romaniv2511/goit-js-hw-09@35a0bb37a753684fba125856c2105d9c483afeb5 🚀

---
+++ 01-color-switcher.f19fe08b.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=null;const o=()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`};t.startBtn.addEventListener("click",(()=>{o(),e=setInterval(o,1e3)})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.f19fe08b.js.map
