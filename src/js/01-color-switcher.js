35a0bb37a753684fba125856c2105d9c483afeb5
romaniv2511 <romaniv2511@gmail.com>
2022-09-29 20:08:10 +0300

    add tast 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+let timerId = null;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onChangeBgColorOfBody = () => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+const onStartBtnClick = () => {
+    onChangeBgColorOfBody();
+    timerId = setInterval(onChangeBgColorOfBody, 1000);
+}
+const onStopBtnClick = () => { clearInterval(timerId) }
+
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
