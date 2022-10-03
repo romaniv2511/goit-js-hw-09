92beb80b77078aabb372de0cf4563e564e0d119b
romaniv2511 <romaniv2511@gmail.com>
2022-10-03 12:36:38 +0300

    disable/enable' for buttons -- tast1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,9 +6,16 @@ const refs = {
 }
 let timerId = null;
 
+onDisableBtn(refs.stopBtn);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function onDisableBtn(btn) {
+    btn.disabled = true;
+}
+function onEnableBtn(btn) {
+    btn.disabled = false;
 }
 
 const onChangeBgColorOfBody = () => {
@@ -17,8 +24,16 @@ const onChangeBgColorOfBody = () => {
 const onStartBtnClick = () => {
     onChangeBgColorOfBody();
     timerId = setInterval(onChangeBgColorOfBody, 1000);
+
+    onDisableBtn(refs.startBtn);
+    onEnableBtn(refs.stopBtn);
 }
-const onStopBtnClick = () => { clearInterval(timerId) }
+const onStopBtnClick = () => { 
+    clearInterval(timerId);
+    
+    onDisableBtn(refs.stopBtn);
+    onEnableBtn(refs.startBtn);
+}
 
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
